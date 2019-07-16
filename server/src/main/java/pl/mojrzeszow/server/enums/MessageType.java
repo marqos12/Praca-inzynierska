74d156d86807fe8796f6129730fdaa78aa3db15d
marqos12 <marqos111222@gmail.com>
2019-07-16 22:14:53 +0200

    #7 adding message types

---
+++ server/src/main/java/pl/mojrzeszow/server/enums/MessageType.java
@@ -0,0 +1,13 @@
+package pl.mojrzeszow.server.enums;
+
+public enum MessageType {
+	GAME_UPDATE,
+	GAME_CONF_UDPATE,
+	GAMERS_STATUS_UPDATE,
+	GTFO_MESSAGE,
+	GAME_START,
+	NEW_TILE,
+	NEXT_ROUND,
+	TILE_CHANGED,
+	GAME_END
+}
